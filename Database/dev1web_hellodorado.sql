8d545546860766b20ca96c5f264d0b004f714a1b
hdGuild <contact.laguilde@helldorado.fr>
2017-02-03 15:41:51 +0000

    added config and testing files

--- Database/dev1web_hellodorado.sql
+++ Database/dev1web_hellodorado.sql
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `dev1web_hellodorado`
+-- Database: `dev1web_helldorado`
 --
 
 -- --------------------------------------------------------
