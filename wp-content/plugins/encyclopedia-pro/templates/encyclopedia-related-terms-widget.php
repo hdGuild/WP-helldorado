7fa51a741ed70409f34e1a6c6de4eb2c740769fd
hdGuild <contact.laguilde@helldorado.fr>
2017-02-08 16:37:06 +0000

    corrected test/ to tests/

--- wp-content/plugins/encyclopedia-pro/templates/encyclopedia-related-terms-widget.php
+++
@@ -1,7 +0,0 @@
-<?php /* Global: $widget, $options */ ?>
-
-<ul class="related-terms">
-	<?php while ($widget->terms->have_Posts()): $widget->terms->the_Post() ?>
-	<li class="term"><a href="<?php the_Permalink() ?>" title="<?php the_Title_Attribute() ?>"><?php the_Title() ?></a></li>
-	<?php endwhile; WP_Reset_Postdata() ?>
-</ul>
