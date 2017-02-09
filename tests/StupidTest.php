e79f08dfd0a52b392e5c6f667bb27432d03030b0
hdGuild <contact.laguilde@helldorado.fr>
2017-02-09 09:11:17 +0000

    added tests/ repertory to repo

---
+++ tests/StupidTest.php
@@ -0,0 +1,11 @@
+<?php
+namespace hd_website\tests;
+
+class StupidTest extends \PHPUnit_Framework_TestCase{
+
+        public function testTrusIsTrue(){
+                $foo = true;
+                $this->assertTrue($foo);
+        }
+}
+
